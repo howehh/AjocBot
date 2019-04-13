76929856914365fdecd6d4dd28a44e33e6c86234
howeh <howehh@yahoo.com>
2019-04-13 12:04:25 -0700

    Reverted tons of commits to remove sensitive info

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // ************************ AJOCBOT MAIN ***********************************
 // The main working file for AjocBot that is ran in Node. Starts process and
-// handles chat message/private message events. Also handles bot shutdown
+// handles chat message/private message events/bot shutdown
 // *************************************************************************
 
 // Node package imports
@@ -10,36 +10,71 @@ const Gists = require('gists');
 const config = require('./config');
 const bot = require('./lib/bot');
 const dates = require('./lib/dates');
-const alarm = require('./lib/alarm'); // alarm has chatMsg (set) and pm (stop) component
-const stream = require('./lib/stream'); // streamerList = chatMsg, refreshStreamers = pm
 const points = require('./lib/points'); // includes two components: trivia and time spent in ajoc
 const count = require('./lib/count');
+const trivia = require('./lib/trivia');
+const store = require('./lib/store');
 
-// ****************************** Chat Msg Handler ******************************* 
-// On a chat message event, data is printed and sent to each chatMsgEvent function
-// *******************************************************************************
+// Arrays of chat msg functions and private message functions:
+// On a chatMsg event or a pm event, each function is called in the respective array
+const chatMsgEvents = []; 
+const whisperEvents = []; 
 
-const chatMsgEvents = []; // Array of chat msg functions
+// Add to the chat message events array
+function addChatEvents(obj) {
+   Object.keys(obj).forEach(key => {
+      if (key.length > 0) { // if the function takes data (0 argument functions aren't chat events)
+         chatMsgEvents.push(obj[key])
+      }
+   });
+}
 
-// Add new chat events to chatMsgEvents array for new features:
+// Add to the whisper events array
+function addWhisperEvents(obj) {
+   Object.keys(obj).forEach(key => {
+      if (key.length > 0) { 
+         whisperEvents.push(obj[key])
+      }
+   });
+}
+
+// Add to both chatMsgEvents and whisperEvents
+function addToChatAndWhispers(obj) {
+   Object.keys(obj).forEach(key => {
+      if (key.length > 0) { 
+         whisperEvents.push(obj[key]);
+         chatMsgEvents.push(obj[key]);
+      }
+   });
+}
+
+// Add new chat events to chatMsgEvents array:
 addChatEvents(require('./lib/basiccommands'));
 addChatEvents(require('./lib/songs'));
 addChatEvents(require('./lib/weather'));
 addChatEvents(require('./lib/convertunits'));
 addChatEvents(require('./lib/define'));
-chatMsgEvents.push(count.counter, count.countReport);
-chatMsgEvents.push(alarm.setAlarm);
-chatMsgEvents.push(stream.streamerList);
-chatMsgEvents.push(points.points, points.playTrivia, points.store, points.secretEmote, points.give,
-   points.addEmote); 
+addChatEvents(count);
+addChatEvents(trivia);
 
-function addChatEvents(obj) {
-   Object.keys(obj).forEach(key => chatMsgEvents.push(obj[key]));
-}
+chatMsgEvents.push(store.store, store.leader, store.addEmote, store.secretEmote);
+chatMsgEvents.push(points.points, points.topPoints, points.give, points.setPoints);
+chatMsgEvents.push(points.startRaffle, points.joinRaffle, points.endRaffle);
 
-bot.socket.on("chatMsg", (data) => {
+// Add new whisper events to whisperEvents array:
+whisperEvents.push(store.secretEmote, store.buySecretEmote, store.confirmSecretEmote);
+
+// Add to both arrays:
+addToChatAndWhispers(require('./lib/alarm'));
+addToChatAndWhispers(require('./lib/notifications'));
+
+// ****************************** Chat Msg Handler ******************************* 
+// On a chat message event, data is printed and sent to each chatMsgEvent function
+// *******************************************************************************
+
+bot.socket.on("chatMsg", data => {
    console.log(data);
-   if (data.username.toLowerCase() === bot.botname) { 
+   if (data.username.toLowerCase() === bot.BOTNAME) { 
       bot.chat("/afk");
       return; // exits this function without sending data to each chatMsgEvent funct
    }
@@ -50,14 +85,9 @@ bot.socket.on("chatMsg", (data) => {
 // ********************************** PM HANDLER *********************************** 
 // On private message events, data is printed and sent to each whisperEvent function
 // *********************************************************************************
-const whisperEvents = [];
-whisperEvents.push(alarm.stopAlarm);
-whisperEvents.push(stream.refreshStreamers); 
-whisperEvents.push(points.secretEmote, points.buySecretEmote, points.confirmSecretEmote);
-
-bot.socket.on("pm", (data) => {
+bot.socket.on("pm", data => {
    console.log(data);
-   if (data.username.toLowerCase() === bot.botname) {
+   if (data.username.toLowerCase() === bot.BOTNAME) {
       return;
    }
    data.msg = data.msg.toLowerCase();
@@ -69,7 +99,7 @@ bot.socket.on("pm", (data) => {
 // ****************************************************************************************
 function saveGistsAndExit() {
    if (count.getCount() > 0 && points.getPointsMapSize() > 0) {
-      console.log("\nSaving Alizee count and Points to gist...");
+      console.log("\nSaving bot data to gist...");
       const gists = new Gists({
          username: config.gist.username,
          password: config.gist.password
@@ -81,11 +111,13 @@ function saveGistsAndExit() {
             },
             "points": {
                "content": points.getPointsMapString()
+            },
+            "trivianum": {
+               "content": "" + trivia.getTriviaNum()
             }
          }
       }
-      gists.edit(config.gist.fileID, options)
-      .then(function() {
+      gists.edit(config.gist.fileID, options).then(() => {
          console.log("\nSave successful.");
          process.exit(0);
       });
@@ -96,22 +128,13 @@ function saveGistsAndExit() {
 }
 
 // On bot disconnecting from cytube, program automatically exits after 10 seconds
-bot.socket.on("disconnect", function() {
-   setTimeout(function() {
-      saveGistsAndExit();
-   }, 10000);
-});
+bot.socket.on("disconnect", () => setTimeout(saveGistsAndExit, 10000));
 
-// On Ctrl+C'ing program, saves Alizee count and user points then exits
-process.on('SIGINT', function () {
-   saveGistsAndExit();
-});
-// On closing command prompt, saves Alizee count and user points then exits
-process.on('SIGHUP', function () {
-   saveGistsAndExit();
-});
+// On Ctrl+C'ing, closing cmd prompt, or terminating, saves Alizee count & points then exits
+['SIGINT', 'SIGHUP', 'SIGTERM'].forEach(signal => process.on(signal, saveGistsAndExit));
+
 // On an error that ends program, saves Alizee count and user points then exits
-process.on('uncaughtException', function(e) {
+process.on('uncaughtException', e => {
    console.log(e.stack);
    console.log();
    saveGistsAndExit();
