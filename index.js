8532df16ff42fb631889a4437cb5cce1722efd99
howeh <howehh@yahoo.com>
2019-04-15 19:29:44 -0700

    Complete refactor: modules add themselves as callbacks instead of index adding them

--- index.js
+++ index.js
@@ -1,98 +1,24 @@
 // ************************ AJOCBOT MAIN ***********************************
-// The main working file for AjocBot that is ran in Node. Starts process and
-// handles chat message/private message events/bot shutdown
+// The main file for AjocBot that is ran in Node. It determines which
+// modules are enabled for AjocBot, and also handles bot shutdown
 // *************************************************************************
-
-// Node package imports
-const Gists = require('gists');
-
-// Bot module imports
 const config = require('./config');
 const bot = require('./lib/bot');
-const dates = require('./lib/dates');
-const points = require('./lib/points'); // includes two components: trivia and time spent in ajoc
+const Gists = require('gists');
+
+// Modules
 const count = require('./lib/count');
+const points = require('./lib/points');
 const trivia = require('./lib/trivia');
-const store = require('./lib/store');
-
-// Arrays of chat msg functions and private message functions:
-// On a chatMsg event or a pm event, each function is called in the respective array
-const chatMsgEvents = []; 
-const whisperEvents = []; 
-
-// Add to the chat message events array
-function addChatEvents(obj) {
-   Object.keys(obj).forEach(key => {
-      if (key.length > 0) { // if the function takes data (0 argument functions aren't chat events)
-         chatMsgEvents.push(obj[key])
-      }
-   });
-}
-
-// Add to the whisper events array
-function addWhisperEvents(obj) {
-   Object.keys(obj).forEach(key => {
-      if (key.length > 0) { 
-         whisperEvents.push(obj[key])
-      }
-   });
-}
-
-// Add to both chatMsgEvents and whisperEvents
-function addToChatAndWhispers(obj) {
-   Object.keys(obj).forEach(key => {
-      if (key.length > 0) { 
-         whisperEvents.push(obj[key]);
-         chatMsgEvents.push(obj[key]);
-      }
-   });
-}
-
-// Add new chat events to chatMsgEvents array:
-addChatEvents(require('./lib/basiccommands'));
-addChatEvents(require('./lib/songs'));
-addChatEvents(require('./lib/weather'));
-addChatEvents(require('./lib/convertunits'));
-addChatEvents(require('./lib/define'));
-addChatEvents(count);
-addChatEvents(trivia);
-
-chatMsgEvents.push(store.store, store.leader, store.addEmote, store.secretEmote);
-chatMsgEvents.push(points.points, points.topPoints, points.give, points.setPoints);
-chatMsgEvents.push(points.startRaffle, points.joinRaffle, points.endRaffle);
-
-// Add new whisper events to whisperEvents array:
-whisperEvents.push(store.secretEmote, store.buySecretEmote, store.confirmSecretEmote);
-
-// Add to both arrays:
-addToChatAndWhispers(require('./lib/alarm'));
-addToChatAndWhispers(require('./lib/notifications'));
-
-// ****************************** Chat Msg Handler ******************************* 
-// On a chat message event, data is printed and sent to each chatMsgEvent function
-// *******************************************************************************
-
-bot.socket.on("chatMsg", data => {
-   console.log(data);
-   if (data.username.toLowerCase() === bot.BOTNAME) { 
-      bot.chat("/afk");
-      return; // exits this function without sending data to each chatMsgEvent funct
-   }
-   data.msg = data.msg.toLowerCase();
-   chatMsgEvents.forEach(c => c(data));
-});
-
-// ********************************** PM HANDLER *********************************** 
-// On private message events, data is printed and sent to each whisperEvent function
-// *********************************************************************************
-bot.socket.on("pm", data => {
-   console.log(data);
-   if (data.username.toLowerCase() === bot.BOTNAME) {
-      return;
-   }
-   data.msg = data.msg.toLowerCase();
-   whisperEvents.forEach(c => c(data));
-});
+require('./lib/alarm');
+require('./lib/basiccommands');
+require('./lib/convertunits');
+require('./lib/dates');
+require('./lib/define');
+require('./lib/store');
+require('./lib/notifications');
+require('./lib/weather');
+require('./lib/songs');
 
 // ******************************** SHUTDOWN HANDLER **************************************
 // When the bot shuts down, relevant data (Alizee count and user points) is saved to a gist
