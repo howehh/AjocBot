638bc5c9f1d3990c9bbf985e8ac7e5f8f11c0c1c
Howie Nguyen <howien@uw.edu>
2019-07-21 17:04:54 -0700

    No longer need to specify socket URL in config

--- index.js
+++ index.js
@@ -2,20 +2,25 @@
 // The main file for AjocBot that is ran in Node. It 
 // determines which modules are enabled for AjocBot
 // *************************************************
-require('./lib/bot');
-require('./lib/shutdown');
 
-// Optional Modules
-require('./lib/count');
-require('./lib/points');
-require('./lib/trivia');
-require('./lib/alarm');
-require('./lib/basiccommands');
-require('./lib/convertunits');
-require('./lib/dates');
-require('./lib/define');
-require('./lib/store');
-require('./lib/notifications');
-require('./lib/weather');
-require('./lib/songs');
-require('./lib/jinx');
+require('./lib/bot').connect(loadModules);
+
+// After connecting to cytube, modules are loaded up
+function loadModules() {
+   require('./lib/shutdown');
+
+   // Optional Modules
+   require('./lib/count');
+   require('./lib/points');
+   require('./lib/trivia');
+   require('./lib/alarm');
+   require('./lib/basiccommands');
+   require('./lib/convertunits');
+   require('./lib/dates');
+   require('./lib/define');
+   require('./lib/store');
+   require('./lib/notifications');
+   require('./lib/weather');
+   require('./lib/songs');
+   require('./lib/jinx');
+}
